f5c1df7c76e505490649dbd27e231c88d168994c
ivanatus <76968401+ivanatus@users.noreply.github.com>
2022-08-24 19:14:49 +0200

    Initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style type="text/css">
+		.header{
+			width: 80%;
+			height: 18%;
+			position: top left;
+			text-align: left;
+			font-size: 50px;
+			border: 1px solid black;
+		}
+
+		.footer{
+			position: bottom center;
+			width: 100%;
+			text-align: left;
+			font-size: 10px;
+			border: 1px solid black;
+		}
+    </style>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet wellness centar</title>
+</head>
+<body>
+	<div class="jumbotron" style="font-family: 'Times New Roman', serif;">
+		<div class="row">
+			<div class="col-md-5 col-sm-5 col-xs-5">
+				<h1 style="font-size:50px">Veterinarska ambulanta Pet Wellness Centar</h1>
+			</div>
+		</div>
+	</div>
+
+	</div class="logo">
+		<img class="thumbnail" src="https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/66456847_2163216043976198_3687836925265182720_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=76wkQjxexlIAX85ft05&_nc_ht=scontent-vie1-1.xx&oh=00_AT9wwGCmabKBxrTHXVOMR2NHV2ftHAvQIaIcHEqHDt3I6A&oe=632A0FC5" height="10%" width="10%">
+	<div>
+
+	
+
+    @yield('content')
+
+   	
+</body>
+</html>
---
+++ resources/views/o-nama.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('content')
+    <div class="container">
+        <h1 style="font-family: 'Times New Roman', serif;">Kontakt</h1>
+        <p class="text-muted">Broj telefona: 051 504 285</p><br>
+        <p class="text-muted">Broj mobitela: 0915099899</p><br>
+        <label>E-mail:</label>  <a class="btn btn-default" href="mailto" style="background-color: lightgray; color: black">zlatkovaadresa@gmail.com</a><br><br>
+  
+        <a href="https://facebook.com/Petwellnesscentar"><img src="https://image.flaticon.com/icons/png/512/124/124010.png" height="5%" width="5%">
+        
+    </div>
+@stop
